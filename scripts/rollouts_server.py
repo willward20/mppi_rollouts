a0731efa83d61a3f16dbb2f5d6cf96949dd45933
William Ward <willward1912@gmail.com>
2025-02-23 15:13:28 -0600

    Correct how the server processes C++ data. Add a new sub/pub for processed odom measurements.

--- scripts/rollouts_server.py
+++ scripts/rollouts_server.py
@@ -68,61 +68,102 @@ def handle_calc_rollouts(req):
         OUTPUTS:
             X = list encoder a (req.K, req.T+1. N) array. 
     """
-    # Print the input constants.
-    print("K: ", req.K)
-    print("T: ", req.T)
-    print("M: ", req.M)
-    print("N: ", req.N)
-    print("-----")
+    # print("[DEBUG]: Function Inputs") 
+    # print("K: ", req.K)
+    # print("T: ", req.T)
+    # print("M: ", req.M)
+    # print("N: ", req.N)
+    # print("len(x0): ", len(req.x0))
+    # print("len(U): ", len(req.U))
+    # print("-----")
 
 
-    # Convert the input lists into torch tensors. 
-    # print("x0: ", len(req.x0))
-    # print("U: ", len(req.U))
+    # Convert x0 and U into torch tensors. Make sure that the 
+    # elements of U are loaded into the tensor correctly (so 
+    # that the tensor matches the ArrayFire array in C++).
     x0 = torch.tensor(req.x0, dtype=torch.float32, device=device)
-    U = torch.tensor(req.U, dtype=torch.float32, device=device).reshape(req.K, req.T+1, req.M) 
-    print("x0: ", x0.shape)
-    print("U: ", U.shape)
+    U = torch.tensor(req.U, dtype=torch.float32, device=device).reshape(req.M, req.T+1, req.K).transpose(1, 2)
+
+    # print("[DEBUG]: Lists Converted to Tensors")
+    # print("Tensor x0: ", x0)
+    # print("x0 shape: ", x0.shape)
+    # print("Tensor U: ", U)
+    # print("U shape: ", U.shape)
 
     # if (T == 0) {
     #     return tile(moddims(x0, 1, 1, N), K);
     # }
 
-    # Define output tensor. 
-    X = torch.zeros((req.K, req.T + 1, req.N), dtype=torch.float32, device=device)
-    print("X: ", X.shape)
+    # Define output tensor size to align with ArrayFire expectations. 
+    X = torch.zeros((req.N, req.K, req.T + 1), dtype=torch.float32, device=device)
 
-    # Define a tensor for the integration time step.
+    # Define a tensor with integration steps for each rollout.
     time = torch.tensor([[req.dT]], device=device)
-    time = time.repeat(req.K, 1) # repeat for each rollout. 
-    print("time: ", time.shape)
+    time = time.repeat(1, req.K) 
 
     # Remove the previous control from the sequence.
-    V = U[:, 1:, :]
-    print("V: ", V.shape)
+    V = U[:, :, 1:]
+
+    # print("[DEBUG]: Tensor Sizes for X, V, and time")
+    # print("X: ", X.shape)
+    # print("time: ", time.shape)
+    # print("V: ", V.shape)
+    # print("V: ", V)
     
-    # Set initial state across all samples at t=0
-    x0_reshaped = x0.reshape(1, 1, req.N) 
-    X[:, 0, :] = x0_reshaped.repeat(req.K, 1, 1).squeeze(1)  # Repeat K times along the first dimension
+    # Set initial state across all samples at t=0. Assign 
+    # the initial state of each sample along X. 
+    x0_reshaped = x0.reshape(req.N, 1, 1) 
+    X[:, :, 0] = x0_reshaped.repeat(1, req.K, 1).squeeze(2) 
+
+    # print("[DEBUG]: Setting the Initial States")
+    # print("x0_reshaped: ", x0_reshaped)
+    # print("x0_reshaped: ", x0_reshaped.shape)
+    # print("x0_reshaped_repeated: ", x0_reshaped.repeat(1, req.K, 1))
+    # print("x0_reshaped_repeated: ", x0_reshaped.repeat(1, req.K, 1).shape)
+    # print("x0_reshaped_repeated_squeezed: ", x0_reshaped.repeat(1, req.K, 1).squeeze(2))
+    # print("X with init cond: ", X)
     
     # Integrate over time. 
     for i in range(req.T):
         # Concatenate all of the inputs.
-        input = torch.cat((time, X[:,i,:], V[:,i,:]), 1).unsqueeze(0)
-        # print("input: ", input.shape)
+        input = torch.cat((time, X[:,:,i], V[:,:,i]), 0)
+        # if i == 0:
+            # print("[DEBUG]: Concatenated Input Tensor")
+            # print("input: ", input)
+            # print("input: ", input.shape)
+
+        # Transpose the inputs to be compatible w/ FEs
+        input = input.transpose(0,1).unsqueeze(0)
+        # if i == 0:
+            # print("[DEBUG]: Transposed Input Tensor")
+            # print("input: ", input)
+            # print("input: ", input.shape)
         
-        # Predict change in states using the model. 
+        # Predict the next state using the model. 
         with torch.no_grad():
             output = model.predict(input, coeff)
-            # print("output: ", output.shape)
+            # if i == 0:
+                # print("[DEBUG]: Output Tensor")
+                # print("output: ", output)
+                # print("output: ", output.shape)
+                # print("output.squeeze(0).transpose(1,0): ", output.squeeze(0).transpose(1,0))
 
-        # Add the change in states to the current state. 
-        X[:,i+1,:] = (X[:,i,:] + output.squeeze(0).squeeze(0)).to("cpu")
+        # Assign the output as the next state. 
+        X[:,:,i+1] = (output.squeeze(0).transpose(1,0)).to("cpu")
+        # if i == 0:
+            # print("[DEBUG]: Assigning the output tensor to X")
+            # print("X: ", X)
 
-    # Flatten the trajectories to a list and respond to the client. 
-    X_flat = X.flatten().tolist()
-    print("X_flat: ", len(X_flat))
-    print("-----------------------")
+    # print("[DEBUG]: Double Check the final shape of X")       
+    # print("X after = ", X.shape)
+
+    # Flatten the trajectories to a list in ROW major order so
+    # that it is easy to unpack into an ArrayFire Array in C++. 
+    X_flat = X.permute(0, 2, 1).contiguous().flatten().tolist()
+
+    # print("[DEBUG]: Check that the tensor was flattened correctly")
+    # print("X_flat: ", X_flat)
+    # print("-----------------------")
     return MppiRolloutsResponse(X_flat)
 
 if __name__ == "__main__":
---
+++ src/odom_processor.cpp
@@ -0,0 +1,66 @@
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
+#include <mppi_rollouts/OdomProcessed.h>
+#include "std_msgs/String.h"
+#include <sstream>
+
+class OdomProcessor
+{
+public:
+    OdomProcessor()
+    {
+        // Initialize subscriber and publisher
+        odom_sub_ = nh_.subscribe("/warty/odom", 10, &OdomProcessor::odomCallback, this);
+        odom_pub_ = nh_.advertise<mppi_rollouts::OdomProcessed>("/warty/odom_processed", 10);
+    }
+
+private:
+    ros::NodeHandle nh_;
+    ros::Subscriber odom_sub_;
+    ros::Publisher odom_pub_;
+
+    void odomCallback(const nav_msgs::Odometry::ConstPtr& odom)
+    {
+        // Debug
+        ROS_INFO("Received odometry message at time: %f", odom->header.stamp.toSec());
+        // ROS_INFO("Position message (x): %f", odom->pose.pose.position.x);
+        
+        // Declare a ROS message for the processed odometry. 
+        mppi_rollouts::OdomProcessed processed_odom;
+
+        // Copy the header from the odom message to preserve time stamps. 
+        // Copy the header from the odom message to preserve time stamps. 
+        // processed_odom.header.stamp = ros::Time::now(); // = odom->header;
+        // processed_odom.header.frame_id = "warty/odom_processed";   // Parent frame
+        // processed_odom.header.stamp = odom->header.stamp;
+        // processed_odom.header.seq = odom->header.seq;
+        // processed_odom.header.stamp = odom->header.stamp;
+        // processed_odom.header.frame_id = odom->header.frame_id;
+
+
+        // Set the time (use the time stamp from the odom topic)
+        processed_odom.time = odom->header.stamp.toSec();
+        
+        // Copy the states. Convert quaternion to yaw angle. 
+        processed_odom.xPos = odom->pose.pose.position.x;
+        processed_odom.yPos = odom->pose.pose.position.y;
+        processed_odom.yaw = tf::getYaw(odom->pose.pose.orientation);
+        processed_odom.xVel = odom->twist.twist.linear.x;
+        processed_odom.zAngVel = odom->twist.twist.angular.z;
+
+        // Debug
+        // ROS_INFO("Processed yaw message: %f", processed_odom.yaw);
+        
+        // Publish processed odometry
+        odom_pub_.publish(processed_odom);
+    }
+};
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "odom_processor");
+    OdomProcessor processor;
+    ros::spin();
+    return 0;
+}
